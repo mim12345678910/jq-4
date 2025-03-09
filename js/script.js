5291fc7f72ca5f7f2ff297ce15f7c177b15f7ee5
mim12345678910 <mim238188@gmail.com>
2025-03-09 20:11:42 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+    wow = new WOW(
+                      {
+                      boxClass:     'wow',      // default
+                      animateClass: 'animated', // default
+                      mobile:       true,       // default
+                      live:         true        // default
+                    }
+                    )
+                    wow.init();
+//jQuery
+$(function(){
+
+
+$('.counter').counterUp({
+delay: 10,
+time: 1000
+
+ });
+});
+
+
